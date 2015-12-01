457f472cec9cdfabefa473d716e1685573697f2b
Hugo-L <hugo.lime@outlook.fr>
2015-12-01 16:29:02 +0100

    1. Modification de la fonction training_sequence pour trouver une séquence avec une bonne autocorrelation. 2. Avancée du rapport
    --> Je pense que ça suffit pas encore, je vais ajouter une phase variable et
    un demodulateur différentiel pour voir ce que ça donne.

--- Digital Communications/Code/simulation.m
+++ Digital Communications/Code/simulation.m
@@ -7,7 +7,7 @@
 clear
 
 % Initialization
-EbN0_db = -10:10;                     % Eb/N0 values to simulate (in dB)
+EbN0_db = 0:10;                     % Eb/N0 values to simulate (in dB)
 nr_bits_per_symbol = 2;             % Corresponds to k in the report
 nr_guard_bits = 10;                 % Size of guard sequence (in nr bits)
                                     % Guard bits are appended to transmitted bits so
@@ -15,8 +15,8 @@ nr_guard_bits = 10;                 % Size of guard sequence (in nr bits)
                                     % of received sequence do not affect the samples
                                     % which contain the training and data symbols.
 nr_data_bits = 1000;                % Size of each data sequence (in nr bits)
-nr_training_bits = 100;             % Size of training sequence (in nr bits)
-nr_blocks = 300;                     % The number of blocks to simulate
+nr_training_bits = 20;             % Size of training sequence (in nr bits)
+nr_blocks = 100;                     % The number of blocks to simulate
 Q = 8;                              % Number of samples per symbol in baseband
 
 % Define the pulse-shape used in the transmitter. 
@@ -37,8 +37,8 @@ square_phase_error = zeros(1, length(EbN0_db)); %phase errors
 forced_errors=0;
 nr_errors_forced = zeros(1, length(forced_errors));   % Error counter
 
-for snr_point = 1:length(EbN0_db)
-  for forced_errror_index = 1:length(forced_errors)
+for snr_point = 1:length(EbN0_db)  
+  for k = 1:length(nr_training_bits)
       % Loop over several blocks to get sufficient statistics.
       for blk = 1:nr_blocks
 
@@ -47,8 +47,8 @@ for snr_point = 1:length(EbN0_db)
         %%%
 
         % Generate training sequence.
-        b_train = training_sequence(nr_training_bits);
-
+        b_train = training_sequence(nr_training_bits(k));        
+        
         % Generate random source data {0, 1}.
         b_data = random_data(nr_data_bits);
 
@@ -92,19 +92,22 @@ for snr_point = 1:length(EbN0_db)
         t_start=Q*(nr_guard_bits-2)/2;
         t_end=t_start+50;
         t_samp = sync(mf, b_train, Q, t_start, t_end);
-        t_samp = 1+Q*nr_guard_bits/2+Q-1+forced_errors(forced_errror_index); %Force the value of t_samp, we can add error here
+        %t_samp = 1+Q*nr_guard_bits/2+Q-1; %Force the value of t_samp, we can add error here
         nr_sync_errors(snr_point) = nr_sync_errors(snr_point) + abs(t_samp-(1+Q*nr_guard_bits/2+Q-1));
 
         % separated by a factor of Q. Only training+data samples are kept.
-        r = mf(t_samp:Q:t_samp+Q*(nr_training_bits+nr_data_bits)/2-1);
+        r = mf(t_samp:Q:t_samp+Q*(nr_training_bits(k)+nr_data_bits)/2-1);
 
 
         % Phase estimation and correction.
         phihat = phase_estimation(r, b_train);
-        phihat=0; %Force the value of phihat, we can add error here
+        %phihat=0; %Force the value of phihat, we can add error here
         square_phase_error(snr_point) = square_phase_error(snr_point)+abs(phihat)^2; 
         r = r * exp(-1i*phihat);
-
+        
+        %Plot constellation
+        %figure()
+        %plot(r);
 
         % Make decisions. Note that dhat will include training sequence bits
         % as well.
@@ -114,9 +117,9 @@ for snr_point = 1:length(EbN0_db)
         % are included in the comparison. The last data bits are missing as well
         % since the whole impulse response due to the last symbol is not
         % included in the simulation program above.
-        temp=bhat(1+nr_training_bits:nr_training_bits+nr_data_bits) ~= b_data;
+        temp=bhat(1+nr_training_bits(k):nr_training_bits(k)+nr_data_bits) ~= b_data;
         nr_errors(snr_point) = nr_errors(snr_point) + sum(temp);
-        nr_errors_forced(forced_errror_index) = nr_errors_forced(forced_errror_index) + sum(temp);
+        %nr_errors_forced(k) = nr_errors_forced(k) + sum(temp);
 
         % Next block.
       end
@@ -140,13 +143,19 @@ BER_forced = nr_errors_forced / nr_data_bits / nr_blocks;
 
 %Display Sync error infos
 % disp(SyncErrorRate);
-% plot(EbN0_db,SyncErrorRate);
+% plot(EbN0_db,SyncErrorRate,'b');
+
 
 %Display phase error infos
-% disp(square_phase_error)
-% plot(EbN0_db,square_phase_error);
+%disp(square_phase_error)
+%plot(EbN0_db,square_phase_error);
 
 %Display BER under added errors
 %disp(BER_forced)
 %plot(forced_errors,BER_forced);
+
+
+%Display influence of training bits
+%plot(nr_training_bits,SyncErrorRate);
+%plot(nr_training_bits,square_phase_error);
 
--- Digital Communications/Code/training_sequence.m
+++ Digital Communications/Code/training_sequence.m
@@ -13,6 +13,29 @@ function b = training_sequence(nr_training_bits)
 %
 % History:
 %   2000-09-28  written /Stefan Parkvall
-%   2001-10-21  modified / George Jöngren
+%   2001-10-21  modified / George Jï¿½ngren
 
-b = (rand(1, nr_training_bits) > .5);
+%b = (rand(1, nr_training_bits) > .5);
+
+
+%Finding a good training sequence
+number_test = 100;
+min_corr=10000; %initial value
+L=nr_training_bits/2;
+for k = 1 :number_test
+    s=(rand(1, 2*L) > .5); %create rand
+    s_qpsk=qpsk(s); 
+    
+    %Calculate autocorrelation difference from a Dirac
+    corr=0;
+    for i=2:(L-1)
+        corr=corr+abs(sum(conj(s_qpsk(i:L)).*s_qpsk(1:(L+1-i)),2)); 
+    end
+    
+    %Find minimum
+    if(corr<min_corr)
+        min_corr=corr;
+        b=s;
+    end 
+    
+end
